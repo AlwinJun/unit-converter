23b9dba25eb4ae0acef02c7c447961cd21bb31a3
AJ <mahidlawonaljun@gmail.com>
2022-08-24 20:17:33 +0800

    Add functionality

---
+++ js/index.js
@@ -0,0 +1,56 @@
+//Generate all conversions when the user click convert button
+//Round the number into 3 decimal places
+
+const inputNumber = document.getElementById('js-input-number')
+const inputButton = document.getElementById('js-input-btn')
+const lengthParagraph = document.getElementById('js-length')
+const volumesParagraph = document.getElementById('js-volumes')
+const massParagraph = document.getElementById('js-mass')
+
+// rendre/call converter function when click
+inputButton.addEventListener('click', function () {
+  METERtoFEET(inputNumber.value)
+  LITERtoGALLON(inputNumber.value)
+  KILOGRAMtoPOUND(inputNumber.value)  
+})
+
+// Empty input field when focus
+inputNumber.addEventListener('focus', function() {
+    inputNumber.value = ' '
+})
+
+/*
+1 meter =  3.28084 feet
+1 liter = 0.2641729 gallon
+1 kilogram = 2.204623 pound
+*/
+const feet =  3.28084
+const gallon = 0.2641729
+const pounds = 2.204623
+
+function METERtoFEET (value) {
+  const meterTOfeet = value * feet
+  const feetTOmeter = value / feet
+  
+  lengthParagraph.innerHTML = 
+  `${value} meters = ${Number(meterTOfeet).toFixed(3)} feet |  
+   ${value} feet =  ${Number(feetTOmeter).toFixed(3)} meters`
+}
+
+function LITERtoGALLON (value) {
+  const litertTOgallon = value * gallon
+  const gallonTOlitter = value / gallon
+
+  volumesParagraph.innerHTML = 
+  `${value} liter = ${Number(litertTOgallon).toFixed(3)} gallons |  
+   ${value} gallons =  ${Number(gallonTOlitter).toFixed(3)} liter`
+}
+
+function KILOGRAMtoPOUND (value) {
+  const kilogramTOpound = value * pounds
+  const poundTOkilogram= value / pounds
+
+  massParagraph.innerHTML = 
+  `${value} kilos = ${Number(kilogramTOpound).toFixed(3)} pounds |  
+   ${value} pounds =  ${Number(poundTOkilogram).toFixed(3)} kilos`
+}
