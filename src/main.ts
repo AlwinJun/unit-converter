c8900d76192075878ade744ace1eb7ee9aa989f5
AJ <mahidlawonaljun@gmail.com>
2023-09-02 15:08:36 +0800

    Refactor code

--- src/main.ts
+++ src/main.ts
@@ -1,62 +1,52 @@
-//Generate all conversions when the user click convert button
-//Round the number into 3 decimal places
+// DOM elements
+const inputNumber = document.getElementById('js-input-number') as HTMLInputElement;
+const lengthParagraph = document.getElementById('js-length') as HTMLParagraphElement;
+const volumesParagraph = document.getElementById('js-volumes') as HTMLParagraphElement;
+const massParagraph = document.getElementById('js-mass') as HTMLParagraphElement;
 
-const inputNumber = document.getElementById(
-  'js-input-number'
-) as HTMLInputElement;
-const lengthParagraph = document.getElementById(
-  'js-length'
-) as HTMLParagraphElement;
-const volumesParagraph = document.getElementById(
-  'js-volumes'
-) as HTMLParagraphElement;
-const massParagraph = document.getElementById(
-  'js-mass'
-) as HTMLParagraphElement;
+function convertUnit(value: number, fromUnit: string, toUnit: string): number {
+  type Units = 'meter' | 'feet' | 'liter' | 'gallon' | 'kilogram' | 'pound';
 
-// Empty input field when focus
+  // Define an object to store the conversion factors
+  const conversionFactors: Record<Units, number> = {
+    meter: 1,
+    feet: 3.28084,
+    liter: 1,
+    gallon: 0.2641729,
+    kilogram: 1,
+    pound: 2.204623,
+  };
+
+  // Check if the fromUnit and toUnit are valid
+  if (!conversionFactors.hasOwnProperty(fromUnit) || !conversionFactors.hasOwnProperty(toUnit)) {
+    throw new Error('Invalid unit');
+  }
+
+  // Calculate the conversion factor by dividing the toUnit  by the fromUnit
+  const conversionFactor = conversionFactors[toUnit as Units] / conversionFactors[fromUnit as Units];
+
+  return value * conversionFactor;
+}
+
 inputNumber.addEventListener('keyup', function () {
   let value: number = Number(inputNumber.value);
-  METERtoFEET(value);
-  LITERtoGALLON(value);
-  KILOGRAMtoPOUND(value);
+
+  //Convert fromUnis to toUnits and vice versa
+  const meterToFeet = convertUnit(value, 'meter', 'feet');
+  const feetToMeter = convertUnit(value, 'feet', 'meter');
+
+  const literToGallon = convertUnit(value, 'liter', 'gallon');
+  const gallonToLiter = convertUnit(value, 'gallon', 'liter');
+
+  const kilogramToPound = convertUnit(value, 'kilogram', 'pound');
+  const poundToKilogram = convertUnit(value, 'pound', 'kilogram');
+
+  lengthParagraph.innerHTML = `${value} meters = ${meterToFeet.toFixed(3)} feet 
+                              | ${value} feet = ${feetToMeter.toFixed(3)} meters`;
+
+  volumesParagraph.innerHTML = `${value} liters = ${literToGallon.toFixed(3)} gallons 
+                              | ${value} gallons = ${gallonToLiter.toFixed(3)} liters`;
+
+  massParagraph.innerHTML = `${value} kilograms = ${kilogramToPound.toFixed(3)} pounds 
+                            | ${value} pounds = ${poundToKilogram.toFixed(3)} kilograms`;
 });
-
-/*
-1 meter =  3.28084 feet
-1 liter = 0.2641729 gallon
-1 kilogram = 2.204623 pound
-*/
-const feet: number = 3.28084;
-const gallon: number = 0.2641729;
-const pounds: number = 2.204623;
-
-function METERtoFEET(value: number): void {
-  const meterTOfeet = value * feet;
-  const feetTOmeter = value / feet;
-
-  lengthParagraph.innerHTML = `${value} meters = ${Number(meterTOfeet).toFixed(
-    3
-  )} feet |
-   ${value} feet =  ${Number(feetTOmeter).toFixed(3)} meters`;
-}
-
-function LITERtoGALLON(value: number): void {
-  const litertTOgallon = value * gallon;
-  const gallonTOlitter = value / gallon;
-
-  volumesParagraph.innerHTML = `${value} liter = ${Number(
-    litertTOgallon
-  ).toFixed(3)} gallons |
-   ${value} gallons =  ${Number(gallonTOlitter).toFixed(3)} liter`;
-}
-
-function KILOGRAMtoPOUND(value: number): void {
-  const kilogramTOpound = value * pounds;
-  const poundTOkilogram = value / pounds;
-
-  massParagraph.innerHTML = `${value} kilos = ${Number(kilogramTOpound).toFixed(
-    3
-  )} pounds |
-   ${value} pounds =  ${Number(poundTOkilogram).toFixed(3)} kilos`;
-}
